5ec420771e4c14698e2d95f25fb6b395da58d2ae
juansebaca <juanseloeb@gmail.com>
2026-04-13 22:49:11 -0300

    Agregacion de imagen y video en index.html y mejora de Modo Oscuro por Juan Sebastian Baca Wayar

--- proyecto/styles/darkmode.js
+++ proyecto/styles/darkmode.js
@@ -1,4 +1,13 @@
-document.getElementById("dark-toggle")
-.addEventListener("click",()=>{
-    document.body.classList.toggle("dark-mode");
+const btnDark = document.getElementById('dark-toggle');
+
+btnDark.addEventListener('click', () => {
+    // 1. Cambia el color de la página
+    document.body.classList.toggle('dark-mode');
+    
+    // 2. Cambia el dibujito del botón
+    if (document.body.classList.contains('dark-mode')) {
+        btnDark.textContent = '☀️'; // Si está oscuro, pone el sol
+    } else {
+        btnDark.textContent = '🌙'; // Si está claro, pone la luna
+    }
 });
